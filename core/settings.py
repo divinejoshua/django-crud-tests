0ea3549bc36c826325a1030a1496b531b236f7a8
divinejoshua <divineuche399@gmail.com>
2023-10-22 15:28:27 +0100

    Added swagger URL

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',       # Swagger
-    'drf_yasg'                      # Yet Another Swagger generator
+    'drf_yasg',                   # Yet Another Swagger generator
     'postapp',
 ]
 
