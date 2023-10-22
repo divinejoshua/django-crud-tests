072f70cbc63c40987b0612a7946785a4e7c90005
divinejoshua <divineuche399@gmail.com>
2023-10-22 15:23:55 +0100

    Installed swagger ui

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',       # Swagger
+    'drf_yasg'                      # Yet Another Swagger generator
     'postapp',
 ]
 
