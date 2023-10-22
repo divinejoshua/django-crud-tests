1e0cedc3585dd3cd401b97e74f0a42c74f184782
divinejoshua <divineuche399@gmail.com>
2023-10-22 15:34:45 +0100

    Added model

--- postapp/models.py
+++ postapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title       = models.CharField(max_length = 180)
+    body        = models.TextField(blank = False)
+    created_at  = models.DateTimeField(auto_now_add= True, auto_now = False, blank = False)
+
+    def __str__(self):
+        return self.title
