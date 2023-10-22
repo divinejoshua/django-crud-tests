9cd95c184288600ce4ee453da2b22ccfe136f487
divinejoshua <divineuche399@gmail.com>
2023-10-22 15:46:46 +0100

    Added CreateBlogPostSerializer

---
+++ postapp/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import BlogPost
+
+class CreateBlogPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'body']
+
+    def save(self):
+        blogpost = BlogPost (title=self.validated_data['title'])
+        title = self.validated_data['title']
+
+        #Add validations before saving
+        if (len(title) >20):                                                                                                             #Make sure that fullname is not more than 50
+            raise serializers.ValidationError({'title' : 'Title cannot be more than 20 characters.'})
+
+        blogpost.save()
+        return blogpost
